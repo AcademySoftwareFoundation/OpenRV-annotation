b1bab41cb1041b92613c37d5354abf3376650748
Roger Nelson <roger.nelson@autodesk.com>
2026-06-08 14:52:10 -0400

    Fix CI: correct CodeQL action ref and clang-format Exc.h
    Signed-off-by: Roger Nelson <roger.nelson@autodesk.com>

--- TwkMath/TwkMath/Exc.h
+++ TwkMath/TwkMath/Exc.h
@@ -12,13 +12,19 @@
 
 // Declare a named exception class derived from BASE_TYPE.
 // Each constructor forwards to BASE_TYPE; the default uses TAG as the message.
-#define TWK_EXC_DECLARE(EXC_TYPE, BASE_TYPE, TAG)         \
-    class EXC_TYPE : public BASE_TYPE                     \
-    {                                                     \
-    public:                                               \
-        EXC_TYPE() : BASE_TYPE(TAG) {}                    \
-        EXC_TYPE(const char* s) : BASE_TYPE(s) {}         \
-        EXC_TYPE(const std::string& s) : BASE_TYPE(s) {}  \
+#define TWK_EXC_DECLARE(EXC_TYPE, BASE_TYPE, TAG)                                                  \
+    class EXC_TYPE : public BASE_TYPE                                                              \
+    {                                                                                              \
+      public:                                                                                      \
+        EXC_TYPE()                                                                                 \
+            : BASE_TYPE(TAG)                                                                       \
+        {}                                                                                         \
+        EXC_TYPE(const char* s)                                                                    \
+            : BASE_TYPE(s)                                                                         \
+        {}                                                                                         \
+        EXC_TYPE(const std::string& s)                                                             \
+            : BASE_TYPE(s)                                                                         \
+        {}                                                                                         \
     };
 
 // Throw EXC_TYPE with the given message string.
@@ -26,12 +32,12 @@
 
 namespace TwkMath {
 
-TWK_EXC_DECLARE(MathExc,           std::runtime_error, "math exception")
-TWK_EXC_DECLARE(SingularMatrixExc, MathExc,            "singular matrix")
-TWK_EXC_DECLARE(BadFrustumExc,     MathExc,            "bad frustum")
-TWK_EXC_DECLARE(MatrixNotAffine,   MathExc,            "matrix not affine")
-TWK_EXC_DECLARE(EigenExc,          MathExc,            "eigen exception")
-TWK_EXC_DECLARE(ZeroScaleExc,      MathExc,            "zero scale matrix")
+TWK_EXC_DECLARE(MathExc, std::runtime_error, "math exception")
+TWK_EXC_DECLARE(SingularMatrixExc, MathExc, "singular matrix")
+TWK_EXC_DECLARE(BadFrustumExc, MathExc, "bad frustum")
+TWK_EXC_DECLARE(MatrixNotAffine, MathExc, "matrix not affine")
+TWK_EXC_DECLARE(EigenExc, MathExc, "eigen exception")
+TWK_EXC_DECLARE(ZeroScaleExc, MathExc, "zero scale matrix")
 
 } // namespace TwkMath
 
